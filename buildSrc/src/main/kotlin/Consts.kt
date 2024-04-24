10a8544c76b97175cb6ec26da352c449e2414170
kristiyan-petrov <kristiyan-petrov@users.noreply.github.com>
2024-04-24 11:32:23 +0000

    Update bom version

--- buildSrc/src/main/kotlin/Consts.kt
+++ buildSrc/src/main/kotlin/Consts.kt
@@ -2,7 +2,7 @@ const val kaleyraLibraryGroup = "com.kaleyra"
 const val publishPath = "releases"
 
 val catalogVersion = "2024.04.04"
-val bomVersion = "4.0.0-rc07"
+val bomVersion = "4.0.0-rc08"
 val mavenAccessKey = runCatching { getLocalProperty("maven.accessKey") }.getOrNull() ?: ""
 val mavenSecretKey = runCatching { getLocalProperty("maven.secretKey") }.getOrNull() ?: ""
 val mavenDistributionId = runCatching { getLocalProperty("maven.distributionId") }.getOrNull() ?: ""
