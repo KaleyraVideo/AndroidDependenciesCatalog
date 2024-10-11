7b616f0177e4acbad2646d38ad2382846af8f21f
Federico Marin <federico.marin@tatacommunications.com>
2024-10-11 10:08:25 +0200

    chore: downgrade comm center version to v14.1.2

--- buildSrc/src/main/kotlin/dependencies/KaleyraDependencies.kt
+++ buildSrc/src/main/kotlin/dependencies/KaleyraDependencies.kt
@@ -21,7 +21,7 @@ object KaleyraDependencies : Dependencies {
             // bandyer
             val kaleyraVideoCoreAvVersion = Version("kaleyraVideoCoreAvVersion", "2.0.0").apply { add(this) }
             val bandyerFirecrasherVersion = Version("bandyerFirecrasherVersion", "1.1.1").apply { add(this) }
-            val kaleyraCommunicationCenterVersion = Version("kaleyraCommunicationCenterVersion", "14.2.0").apply { add(this) }
+            val kaleyraCommunicationCenterVersion = Version("kaleyraCommunicationCenterVersion", "14.1.2").apply { add(this) }
             val kaleyraAudioSessionVersion = Version("kaleyraAudioSessionVersion", "5.0.1").apply { add(this) }
 
             // kaleyra
