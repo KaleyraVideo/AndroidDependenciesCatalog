dfc2f32541ce8d782d8970f97e3413d6832ffb23
fede987 <fede987@users.noreply.github.com>
2024-12-04 11:03:38 +0000

    Update catalog version

--- buildSrc/src/main/kotlin/Consts.kt
+++ buildSrc/src/main/kotlin/Consts.kt
@@ -1,7 +1,7 @@
 const val kaleyraLibraryGroup = "com.kaleyra"
 const val publishPath = "releases"
 
-val catalogVersion = "2024.11.02"
+val catalogVersion = "2024.12.00"
 val bomVersion = "4.2.1"
 val mavenAccessKey = runCatching { getLocalProperty("maven.accessKey") }.getOrNull() ?: ""
 val mavenSecretKey = runCatching { getLocalProperty("maven.secretKey") }.getOrNull() ?: ""
