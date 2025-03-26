8c5de4641eb94c3736d612406487dd6b786a3e54
fede987 <137904192+fede987@users.noreply.github.com>
2025-03-26 09:55:16 +0000

    Update bom version

--- buildSrc/src/main/kotlin/Consts.kt
+++ buildSrc/src/main/kotlin/Consts.kt
@@ -2,7 +2,7 @@ const val kaleyraLibraryGroup = "com.kaleyra"
 const val publishPath = "releases"
 
 val catalogVersion = "2025.03.05"
-val bomVersion = "4.6.1"
+val bomVersion = "4.7.0"
 val mavenAccessKey = runCatching { getLocalProperty("maven.accessKey") }.getOrNull() ?: ""
 val mavenSecretKey = runCatching { getLocalProperty("maven.secretKey") }.getOrNull() ?: ""
 val mavenDistributionId = runCatching { getLocalProperty("maven.distributionId") }.getOrNull() ?: ""
