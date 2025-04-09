47f12a03a4af13cfdc3c21de599f5024393910ab
stefanobrusa <71256468+stefanobrusa@users.noreply.github.com>
2025-04-09 09:33:33 +0000

    Update library kaleyraVideoUtilsVersion to 2.1.0

--- buildSrc/src/main/kotlin/dependencies/KaleyraDependencies.kt
+++ buildSrc/src/main/kotlin/dependencies/KaleyraDependencies.kt
@@ -27,7 +27,7 @@ object KaleyraDependencies : Dependencies {
             // kaleyra
             val kaleyraChatSdkVersion = Version("kaleyraChatSdkVersion", "1.0.0-alpha15").apply { add(this) }
             val kaleyraVideoVersion = Version("kaleyraVideoVersion", "1.3.2").apply { add(this) }
-            val kaleyraVideoUtilsVersion = Version("kaleyraVideoUtilsVersion", "2.0.1").apply { add(this) }
+            val kaleyraVideoUtilsVersion = Version("kaleyraVideoUtilsVersion", "2.1.0").apply { add(this) }
             val kaleyraVideoNetworkingVersion = Version("kaleyraVideoNetworkingVersion", "4.1.0").apply { add(this) }
             val kaleyraSocketIoVersion = Version("kaleyraSocketIoVersion", "2.0.1").apply { add(this) }
             val kaleyraMaterialColorUtilitiesVersion = Version("kaleyraMaterialColorUtilitiesVersion", "2024.08.01").apply { add(this) }
